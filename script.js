62594ed91159abe72b3dd7532d590a85e7cc55e2
Jim Poulin <jpoulin587a@gmail.com>
2021-06-18 16:44:41 -0500

    added code to pull events from local storage

--- script.js
+++ script.js
@@ -15,6 +15,18 @@ function displayTime() {
   });
 
 
+ // get event data from local storage (https://www.w3schools.com/jquery/html_val.asp)
+$("#event08").val(localStorage.getItem("hr08"));
+$("#event09").val(localStorage.getItem("hr09"));
+$("#event10").val(localStorage.getItem("hr10"));
+$("#event11").val(localStorage.getItem("hr11"));
+$("#event12").val(localStorage.getItem("hr12"));
+$("#event01").val(localStorage.getItem("hr01"));
+$("#event02").val(localStorage.getItem("hr02"));
+$("#event03").val(localStorage.getItem("hr03"));
+$("#event04").val(localStorage.getItem("hr04"));
+$("#event05").val(localStorage.getItem("hr05"));
+$("#event06").val(localStorage.getItem("hr06"));
 
 //run the date display function on page load
 displayTime();

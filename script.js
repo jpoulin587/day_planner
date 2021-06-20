83dff932a9db0edf3c72fde04ceffe0d95908ad1
Jim Poulin <jpoulin587a@gmail.com>
2021-06-20 15:21:31 -0500

    color change with time change works

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function hourIndicator() {
 
 
 // save data from the form to local storage
-  $(".savebtn").on("click", function() {
+  $(".saveBtn").on("click", function() {
     let eventText = $(this).siblings(".text-form").val();
     let timeSlot = $(this).siblings().attr("id");
 
@@ -41,8 +41,85 @@ $("#event06").val(localStorage.getItem("hr06"));
 displayTime();
 hourIndicator();
 
-// change the color of the hour bubbles for the current hour
+
+// change the color of the hour bubbles for the current hour (https://www.w3schools.com/jsref/met_element_setattribute.asp)
 if (currentHour > 8) {
   console.log("it's past 8");
-  document.getElementById("hr08").setAttribute("style", "background-color: pink;");
-}
+  //document.getElementById("hr08").setAttribute("style", "background-color: pink;");
+  document.getElementById("hr08").style.backgroundColor = "pink"
+} else if (currentHour === 8) {
+  console.log("it's currently 8");
+  document.getElementById("hr08").style.backgroundColor = "white"
+};
+
+if (currentHour > 9) {
+  document.getElementById("hr09").style.backgroundColor = "pink"
+} else if (currentHour === 9) {
+  console.log("it's currently 9");
+  document.getElementById("hr09").style.backgroundColor = "white"
+};
+
+if (currentHour > 10) {
+  document.getElementById("hr10").style.backgroundColor = "pink"
+} else if (currentHour === 10) {
+  console.log("it's currently 10");
+  document.getElementById("hr10").style.backgroundColor = "white"
+};
+
+if (currentHour > 11) {
+  document.getElementById("hr11").style.backgroundColor = "pink"
+} else if (currentHour === 11) {
+  console.log("it's currently 11");
+  document.getElementById("hr11").style.backgroundColor = "white"
+};
+
+if (currentHour > 12) {
+  document.getElementById("hr12").style.backgroundColor = "pink"
+} else if (currentHour === 12) {
+  console.log("it's currently 12");
+  document.getElementById("hr12").style.backgroundColor = "white"
+};
+
+if (currentHour > 13) {
+  document.getElementById("hr01").style.backgroundColor = "pink"
+} else if (currentHour === 13) {
+  console.log("it's currently 1:00");
+  document.getElementById("hr01").style.backgroundColor = "white"
+};
+
+if (currentHour > 14) {
+  document.getElementById("hr02").style.backgroundColor = "pink"
+} else if (currentHour === 14) {
+  console.log("it's currently 2:00");
+  document.getElementById("hr02").style.backgroundColor = "white"
+};
+
+if (currentHour > 15) {
+  document.getElementById("hr03").style.backgroundColor = "pink"
+} else if (currentHour === 15) {
+  console.log("it's currently 03");
+  document.getElementById("hr03").style.backgroundColor = "LightGreen"
+};
+
+if (currentHour > 16) {
+  document.getElementById("hr04").style.backgroundColor = "pink"
+} else if (currentHour === 16) {
+  console.log("it's currently 4");
+  document.getElementById("hr04").style.backgroundColor = "white"
+};
+
+if (currentHour > 17) {
+  document.getElementById("hr05").style.backgroundColor = "pink"
+} else if (currentHour === 17) {
+  console.log("it's currently 5");
+  document.getElementById("hr05").style.backgroundColor = "white"
+};
+
+if (currentHour > 18) {
+  document.getElementById("hr06").style.backgroundColor = "pink"
+} else if (currentHour === 18) {
+  console.log("it's currently 6");
+  document.getElementById("hr06").style.backgroundColor = "white"
+};
+
+
